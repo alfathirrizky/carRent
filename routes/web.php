06eefe561c2540156a96a86c93f4d8b9bfd5ead3
alfathir rizky harsya <alfathirrizky03@gmail.com>
2025-04-18 02:21:11 +0700

    update page

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/cars', function () {
+    return view('listCars');
+});
+Route::get('/location', function () {
+    return view('location');
+});
+Route::get('/contact us', function () {
+    return view('contactUs');
+});
